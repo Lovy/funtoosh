3561223126d1f58fe75460d0805b87aa9684f282
Lovy Gupta <loveygupta007@gmail.com>
2014-03-10 12:13:56 +0530

    fbtest

--- codeigniter/hugde/controllers/logout.php
+++ codeigniter/hugde/controllers/logout.php
@@ -5,9 +5,16 @@ class logout extends CI_Controller{
 	function __construct(){
 		parent::__construct();
 		$this->load->helper('utility');
+		//Facebook related
+		parse_str( $_SERVER['QUERY_STRING'], $_REQUEST );
+        $CI = & get_instance();
+		$CI->config->load("facebook",TRUE);
+		$config = $CI->config->item('facebook');
+		$this->load->library('Facebook', $config);
 	}
 	
 	function index(){
+		$this->facebook->destroySession();
 		$this->session->sess_destroy();
 		$url = base_url().'/home';
         redirect($url);
