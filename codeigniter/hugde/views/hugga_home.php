ff77ba7c2a0be9b77815b3fe178a97f0c1f6f3a3
Lovy Gupta <loveygupta007@gmail.com>
2014-03-17 02:03:36 +0530

    tweet button

--- codeigniter/hugde/views/hugga_home.php
+++ codeigniter/hugde/views/hugga_home.php
@@ -268,7 +268,7 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
                            	
                            		<!--<a href="https://twitter.com/share?count=horizontal" class="twitter-share-button" data-lang="en">Tweet</a>
                            		<!--<div class="pull-right" style="margin-top:-25px"><i class="icon-calendar"></i> <a href="#">April 16, 2013</a></div>-->
-                           	<a href="https://twitter.com/share?count=horizontal" class="btn btn-info"><i class="icon-twitter"></i> Twitter</a>
+                           	<a href="#" onclick="window.open('https://twitter.com/share?url=http%3A%2F%2Fhugde.com%2Fhugga%2F<?php echo $item['huggaId'];?>','width=626,height=436');return false;" class="btn btn-info"><i class="icon-twitter"></i> Twitter</a>
                            	<a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fhugde.com%2Fhugga%2F<?php echo $item['huggaId'];?>','facebook-share-dialog','width=626,height=436');return false;" class="btn btn-primary"><i class="icon-facebook"></i> Facebook</a>
                            	<a href="https://plus.google.com/share?url=http%3A%2F%2Fhugde.com%2Fhugga%2F<?php echo $item['huggaId'];?>" onclick="javascript:window.open(this.href,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" class="btn btn-danger"><i class="icon-google-plus"></i> Google Plus</a>
                            	
