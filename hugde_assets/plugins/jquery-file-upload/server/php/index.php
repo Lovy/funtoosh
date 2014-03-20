74e72f37f8374f152f364d82588eeabf0c99c1c9
Lovy Gupta <loveygupta007@gmail.com>
2014-03-20 17:27:41 +0530

    thumbnail upload

--- hugde_assets/plugins/jquery-file-upload/server/php/index.php
+++ hugde_assets/plugins/jquery-file-upload/server/php/index.php
@@ -54,12 +54,12 @@ class CustomUploadHandler extends UploadHandlerS3 {
         if (empty($file->error)) {
         	//var_dump($file);
         	
-            $sql = 'INSERT INTO '.$this->options['db_table'].' (originalImageUrl,size,type,name)'.' VALUES (?,?, ?,?)';
+            $sql = 'INSERT INTO '.$this->options['db_table'].' (originalImageUrl,thumbnailImageUrl,size,type,name)'.' VALUES (?,?,?, ?,?)';
             $query = $this->db->prepare($sql);
             $query->bind_param(
-                'siss',
+                'ssiss',
                 $file->url,
-                //$file->thumbnailUrl,
+                $file->thumbnailUrl,
                 $file->size,
                 $file->type,
                 $file->name
