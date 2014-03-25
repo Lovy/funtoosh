117874474861c0a72092407b45ac7b372c80581f
Lovy Gupta <loveygupta007@gmail.com>
2014-03-25 20:26:43 +0530

    fb autologin

--- hugde_assets/scripts/facebook.js
+++ hugde_assets/scripts/facebook.js
@@ -46,6 +46,22 @@
 		    }
 		},{scope:'email'});
 	});
+	
+	FB.getLoginStatus(function(response) {
+	  if (response.status === 'connected') {
+	    // the user is logged in and has authenticated your
+	    // app, and response.authResponse supplies
+	    // the user's ID, a valid access token, a signed
+	    // request, and the time the access token 
+	    // and signed request each expire
+	    getData();
+	  } else if (response.status === 'not_authorized') {
+	    // the user is logged in to Facebook, 
+	    // but has not authenticated your app
+	  } else {
+	    // the user isn't logged in to Facebook.
+	  }
+	 });
 		
   // Here we run a very simple test of the Graph API after login is successful. 
   // This testAPI() function is only called in those cases. 
