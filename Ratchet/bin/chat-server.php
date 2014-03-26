e82f7a052819c882cd86081138f253844ac46cb3
Lovy Gupta <loveygupta007@gmail.com>
2014-03-26 17:31:59 +0530

    ratchet added

---
+++ Ratchet/bin/chat-server.php
@@ -0,0 +1,14 @@
+<?php
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
+use MyApp\Chat;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+$server = IoServer::factory(
+    new WsServer(
+        new Chat()
+    ), 8080
+);
+
+$server->run();
