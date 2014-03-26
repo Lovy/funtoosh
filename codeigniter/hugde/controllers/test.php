b634d043e64f295e7104d53fa129af54646c460d
Lovy Gupta <loveygupta007@gmail.com>
2014-03-26 19:34:00 +0530

    ratchet added

--- codeigniter/hugde/controllers/test.php
+++ codeigniter/hugde/controllers/test.php
@@ -2,22 +2,266 @@
 
 class test extends CI_Controller{
 	
-	function index(){
-	
+	function __construct(){
+		parent::__construct();
 		$this->load->helper('utility');
 		$this->load->library('session');
-		$data = $this->session->all_userdata();
-		var_dump($data);
-		//date in mm/dd/yyyy format; or it can be in other formats as well
-		$birthDate = date('m/d/Y',strtotime("17-2-1983"));
-		//echo $birthDate;
-  //$birthDate = "2/17/1983";
-  //explode the date to get month, day and year
-  $birthDate = explode("/", $birthDate);
-  //get age from date or birthdate
-  $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
-    ? ((date("Y") - $birthDate[2]) - 1) : (date("Y") - $birthDate[2]));
-  echo "Age is:" . $age;
+		$this->load->library('Mobile_Detect');
+	}
+	
+	function index(){
+		$this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate, no-transform, max-age=0, post-check=0, pre-check=0");
+		$this->output->set_header("Pragma: no-cache");
+		$LoginFlag = $this->session->userdata('IsLoggedIn');
+		if(!empty($LoginFlag)){
+			$data = $this->session->all_userdata();
+			//var_dump($data);
+			$this->load->model('modelhome');
+			$response['huggas'] = $this->modelhome->loadData('0',$data['userId'],'HIDE',4,1,'ALL');   //(huggasPerPage,pageNo)
+			$response['sidebar'] = $this->modelhome->loadSideBar();
+			$response['data']=$data;
+			$response['category']='ALL';
+			//echo json_encode($response);
+			
+			//Detect mobile and load no-sidebar version
+			$mobile = $this->mobile_detect->isMobile();
+			if($mobile){
+				$this->load->view('hugga_home_mobile',$response);
+			}
+			else{
+				$this->load->view('testview',$response);
+			}
+			
+		}
+		else{
+			$this->load->model('modelhome');
+			$response['huggas'] = $this->modelhome->loadData('0',NULL,'HIDE',4,1,'ALL');
+			$response['sidebar'] = $this->modelhome->loadSideBar();
+			$response['data']=array("userId"=>"0");
+			$response['category']='ALL';
+			//echo json_encode($response['sidebar']);
+			//var_dump($response);
+			
+			//Detect mobile and load no-sidebar version
+			$mobile = $this->mobile_detect->isMobile();
+			if($mobile){
+				$this->load->view('hugga_home_mobile',$response);
+			}
+			else{
+				$this->load->view('testview',$response);
+			}
+			
+		}
+	}
+
+	function delete($huggaId){
+		$LoginFlag = $this->session->userdata('IsLoggedIn');
+		if(!empty($LoginFlag)){
+			$data = $this->session->all_userdata();
+			//var_dump($data);
+			$this->load->model('modelhome');
+			$this->modelhome->deleteHugga($huggaId);   //(huggasPerPage,pageNo)
+			$url = 'http://hugde.com/home/myhugge';
+			redirect ($url);
+		}
+	}
+	
+	function autoload($category){
+		$huggasPerPage = $this->input->post('HPP');
+		$pageNo = $this->input->post('PN');
+		$LoginFlag = $this->session->userdata('IsLoggedIn');
+		if(!empty($LoginFlag)){
+			$data = $this->session->all_userdata();
+			//var_dump($data);
+			$this->load->model('modelhome');
+			$response['huggas'] = $this->modelhome->loadData('0',$data['userId'],'HIDE',$huggasPerPage,$pageNo,$category);   //(huggasPerPage,pageNo)
+			$response['data']=$data;
+			//var_dump($response['data']);
+			//return HTML code
+			//echo json_encode($response);
+			if(!empty($response['huggas'])){
+				echo $this->jsonToHtml($response);
+			}
+			
+			
+		}
+		else{
+			$this->load->model('modelhome');
+			$response['data']=array("userId"=>"0");
+			$response['huggas'] = $this->modelhome->loadData('0',NULL,'HIDE',$huggasPerPage,$pageNo,$category);
+			
+			//return HTML code
+			if(!empty($response['huggas'])){
+				echo $this->jsonToHtml($response);
+			}
+			
+		}
+	}
+	
+	function autoloadMyhugge($category){
+		$huggasPerPage = $this->input->post('HPP');
+		$pageNo = $this->input->post('PN');
+		$LoginFlag = $this->session->userdata('IsLoggedIn');
+		if(!empty($LoginFlag)){
+			$data = $this->session->all_userdata();
+			//var_dump($data);
+			$this->load->model('modelhome');
+			$response['huggas'] = $this->modelhome->loadData('0',$data['userId'],'SHOW',$huggasPerPage,$pageNo,$category);   //(huggasPerPage,pageNo)
+			$response['data']=$data;
+			//var_dump($response['data']);
+			//return HTML code
+			//echo json_encode($response);
+			if(!empty($response['huggas'])){
+				echo $this->jsonToHtml($response);
+			}
+			
+			
+		}
+		else{
+			$this->load->model('modelhome');
+			$response['data']=array("userId"=>"0");
+			$response['huggas'] = $this->modelhome->loadData('0',NULL,'SHOW',$huggasPerPage,$pageNo,$category);
+			
+			//return HTML code
+			if(!empty($response['huggas'])){
+				echo $this->jsonToHtml($response);
+			}
+			
+		}
+	}
+	
+	function jsonToHtml($data){
+		$x='';
+		foreach($data['huggas'] as $item){
+			if($item['lick']['licked']==1)
+			{$y='green';}
+			else{$y='default';}
+			
+			if($item['flush']['flushed']==1)
+			{$z='red';}
+			else{$z='default';}
+			
+			if($item['flush']['flushed']==1) {$a='style="opacity: 0.3"';}else{$a='';}
+			if($item['lick']['licked']==1) {$b='style="opacity: 0.3"';}else{$b='';}	
+		$x.='	
+		<div class="row">
+                  		<div class="col-md-12">
+                  			<div class="clearfix">                 				
+                  			<!------------In case already licked by the user then the id - licked else unlicked. Similar for flush --------
+                  			lick(userId,HuggaId)
+                  			-->	
+                  			<a href="javascript:void(0);" onclick="lick('.$data['data']['userId'].','.$item["huggaId"].',this);" '.$a.'  class="btn '.$y.' lick" id="licked">Lick <i class="icon-chevron-up"></i> <span class="badge badge-danger">'.$item['licks'].'</span></a>                			
+                  			<a href="javascript:void(0);" onclick="flush('.$data['data']['userId'].','.$item['huggaId'].',this);" '.$b.' class="btn '.$z.' flush" id="flushed">Flush <i class="icon-chevron-down"></i> <span class="badge badge-success">'.$item['flushes'].'</span></a>                           
+                  			</div>		
+                  		</div>	
+                  	</div>
+                  	 <a href="'.base_url().'hugga/'.$item['huggaId'].'"><h3 style="font-weight: 600 !important">'.$item['title'].'</h3></a>
+                     <div class="blog-tag-data">
+                     	<a href="'.base_url().'hugga/'.$item['huggaId'].'"><img src="http://d2nds2wyuzde9r.cloudfront.net/hugde_assets/img/longLoader.gif" onload="this.src=\''. $item['images'][0]['originalImageUrl'].'\'" class="img-responsive" alt="" style="width:100%"></a>
+                        <div class="row">
+                           <!--<div class="col-md-2">
+                           	
+                           		<a href="https://twitter.com/share?count=horizontal" class="twitter-share-button" data-lang="en">Tweet</a>
+                           		<div class="pull-right" style="margin-top:-25px"><i class="icon-calendar"></i> <a href="#">April 16, 2013</a></div>
+                           
+                           	</div>
+                           	<div class="col-md-4">
+                           	  	<div class="fb-like" data-href="'.base_url().'hugga/'.$item['huggaId'].'" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true" style="width: 150px !important"></div>
+                           		
+                          	</div>-->
+                          	<div class="col-md-12">
+                           	
+                           		<!--<a href="https://twitter.com/share?count=horizontal" class="twitter-share-button" data-lang="en">Tweet</a>
+                           		<!--<div class="pull-right" style="margin-top:-25px"><i class="icon-calendar"></i> <a href="#">April 16, 2013</a></div>-->
+                           	<a href="https://twitter.com/share?count=horizontal" class="btn btn-info"><i class="icon-twitter"></i> Twitter</a>
+                           	<a href="#" onclick="window.open(\'https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fhugde.com%2Fhugga%2F'.$item['huggaId'].'\',\'facebook-share-dialog\',\'width=626,height=436\');return false;" class="btn btn-primary"><i class="icon-facebook"></i> Facebook</a>
+                           	<a href="https://plus.google.com/share?url=http%3A%2F%2Fhugde.com%2Fhugga%2F'.$item['huggaId'].'" onclick="javascript:window.open(this.href,\'\',\'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600\');return false;" class="btn btn-danger"><i class="icon-google-plus"></i> Google Plus</a>
+                           	
+                           	</div>
+                           	<script type="text/javascript">
+								  (function() {
+								    var po = document.createElement(\'script\'); po.type = \'text/javascript\'; po.async = true;
+								    po.src = \'https://apis.google.com/js/plusone.js\';
+								    var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(po, s);
+								  })();
+								</script>
+                          	<!--<div class="col-md-3">
+                          		Posted by: '. $item['postedBy'].'
+                          	</div>
+                          	<div class="col-md-3" style="font-size: 18px">
+                          		Views '.$item['views'].'
+                          	</div>-->
+                          	
+                        </div>
+                        <div class="row" style="margin-top: 5px">
+                        	<div class="col-md-12">
+                        		<div class="fb-like" data-href="'.base_url().'hugga/'.$item['huggaId'].'" data-layout="standard" data-action="like" data-show-faces="false" data-share="false"></div>
+                        	</div>
+                        </div>
+                     </div>
+                     <!--end news-tag-data-->
+                    
+                    <hr>
+';
+}
+	return $x;
+	}
+	
+	function totalHuggas(){
+		$this->load->model('modelhome');
+		$result = $this->modelhome->totalHuggas();
+		echo json_encode($result);
+	}
+	
+	function myhugge(){
+		
+		$LoginFlag = $this->session->userdata('IsLoggedIn');
+		if(!empty($LoginFlag)){
+			$userId = $this->session->userdata('userId');
+			$data = $this->session->all_userdata();
+			$this->load->model('modelhome');
+			//loadData($huggaId=NULL,$userId=NULL,$myhugga=NULL,$huggasPerPage=NULL,$pageNo=NULL)
+			$response['huggas'] = $this->modelhome->loadData('0',$data['userId'],'SHOW',100,1,'ALL');
+			$response['sidebar'] = $this->modelhome->loadSideBar();
+			$response['data']=$data;
+			//Detect mobile and load no-sidebar version
+			$mobile = $this->mobile_detect->isMobile();
+			if(is_null($response['huggas'])){
+				$this->load->view('myHuggasEmptyPage',$response);
+			}else{
+				if($mobile){
+				$this->load->view('myHuggas',$response);
+				}
+				else{
+					//var_dump($response);
+					//$this->load->view('hugga_home_myhuggas',$response);
+					$this->load->view('myHuggas',$response);
+				}
+			}
+			
+		}
+		else{
+			echo "Not logged in";
+		}
+	}
+	
+	function next($huggaId,$userId){
+		
+		// seed with microseconds
+		$this->load->model('modelhome');
+		
+		$result = $this->modelhome->getNextHugga($huggaId,$userId);
+		echo json_encode($result);
+	}
+	
+	function flag($huggaId){
+		$userId = $this->session->userdata('userId');
+		$response = $this->input->post('temp');
+		parse_str($response,$output);
+		$feedback=$output['feedback'];
+		$this->load->model('modellickflush');
+		$this->modellickflush->updateFlagCount($huggaId);
+		$this->modellickflush->insertFlag($huggaId,$userId,$feedback);	
 	}
 }
 
