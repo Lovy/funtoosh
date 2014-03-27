4e4681bf26809332c3dd6368c78e64bc3c027c48
Lovy Gupta <loveygupta007@gmail.com>
2014-03-27 21:57:30 +0530

    testview

--- Ratchet/bin/chat-server.php
+++ Ratchet/bin/chat-server.php
@@ -13,6 +13,7 @@
     $pull->on('message', array($pusher, 'onBlogEntry'));
 
     // Set up our WebSocket server for clients wanting real-time updates
+    /*
     $webSock = new React\Socket\Server($loop);
     $webSock->listen(8080, '0.0.0.0'); // Binding to 0.0.0.0 means remotes can connect
     $webServer = new Ratchet\Server\IoServer(
@@ -25,7 +26,16 @@
         ),
         $webSock
     );
+	*/
+	
+	$server = IoServer::factory(
+    new WsServer(
+    	 new Ratchet\Wamp\WampServer(
+        	new Pusher()
+		)
+    ), 8080
+	);
 
-    $loop->run();
+    $server->run();
 	
 ?>
