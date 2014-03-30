b09137adf7e2a186cedf7f3ab37cc67e2fed0414
Lovy Gupta <loveygupta007@gmail.com>
2014-03-31 05:06:14 +0530

    create meme

--- codeigniter/hugde/views/hugga_home.php
+++ codeigniter/hugde/views/hugga_home.php
@@ -12,7 +12,7 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
 <!-- BEGIN HEAD -->
 <head>
    <meta charset="utf-8" />
-   <title>Hugde | Making sh*t awesome</title>
+   <title>Hugde | Making sh*t awesome Yami Gautam Memes</title>
    <meta name="description" content="Yami Gautam memes and jokes.">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
