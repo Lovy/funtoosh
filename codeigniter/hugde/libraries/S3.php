346166bcfa44877cf64abfc8d3ab507bbda3d833
Lovy Gupta <loveygupta007@gmail.com>
2014-03-30 10:18:59 +0530

    create hugga

--- codeigniter/hugde/libraries/S3.php
+++ codeigniter/hugde/libraries/S3.php
@@ -304,7 +304,7 @@ class S3 {
 			foreach ($metaHeaders as $h => $v) $rest->setAmzHeader('x-amz-meta-'.$h, $v);
 			$rest->getResponse();
 		} else
-			echo $rest->size.''.$rest->data.''.$rest->fp;
+			echo "size==".$rest->size.' data='.$rest->data.' fp'.$rest->fp;
 			$rest->response->error = array('code' => 0, 'message' => 'Missing input parameters');
 
 		if ($rest->response->error === false && $rest->response->code !== 200)
