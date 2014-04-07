84e91cdac9ecd204b310ffcd93eec305f208b2a9
Lovy Gupta <loveygupta007@gmail.com>
2014-04-07 17:04:37 +0530

    tag testing

--- codeigniter/hugde/controllers/tags.php
+++ codeigniter/hugde/controllers/tags.php
@@ -50,7 +50,7 @@ class tags extends CI_Controller{
 			$response['tags'] = $this->modeltag->loadTopTags();
 			$response['data']=array("userId"=>"0");
 			$response['category']=$tagName;
-			//echo json_encode($response['sidebar']);
+			echo json_encode($response);
 			//var_dump($response);
 			
 			//Detect mobile and load no-sidebar version
@@ -60,7 +60,7 @@ class tags extends CI_Controller{
 					$this->load->view('hugga_home_mobile',$response);
 				}
 				else{
-					$this->load->view('hugga_home_test',$response);
+					//$this->load->view('hugga_home_test',$response);
 				}
 			}else{
 				$this->load->view('error',$response);
