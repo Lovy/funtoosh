fdac63babbc832e5634b0845e320ec4be006cd91
Lovy Gupta <loveygupta007@gmail.com>
2014-04-07 11:54:30 +0530

    tag testing

--- hugde_assets/scripts/form-components.js
+++ hugde_assets/scripts/form-components.js
@@ -87,7 +87,7 @@ var FormComponents = function () {
 				},
 				success:function(data){
 					//console.log(data);
-					if(data != 'null'){
+					if(typeof(data) != 'undefined' && data != 'null'){
 						if(data['response']==0){
 						$(".tagMappingError").css('display','block');
 						}
